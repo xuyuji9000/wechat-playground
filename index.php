1717523bf548ba7684981ae6b247ffc7122d165b
徐余吉 <yogiman@xuyujis-MBP.lan>
2018-03-20 13:01:42 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+use EasyWeChat\Factory;
+
+$dotenv = new Dotenv\Dotenv(__DIR__);
+$dotenv->load();
+
+$config = [
+    'app_id' => getenv('APP_ID'),
+    'secret' => getenv('SECRET'),
+
+    'response_type' => 'array',
+
+    // 'log' => [
+    //     'level' => 'debug',
+    //     'file' => __DIR__.'/wechat.log',
+    // ],
+];
+
+$app = Factory::officialAccount($config);
+
+$app->server->push(function ($message) {
+    return "您好！欢迎使用 EasyWeChat!";
+});
+
+$response = $app->server->serve();
+
+// 将响应输出
+$response->send(); // Laravel 里请使用：return $response;
