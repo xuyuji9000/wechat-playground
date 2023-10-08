de5eb64020629fd5e6b3e84a9dbc13741ca1a17e
Karl Xu <karl.yogi.xu@gmail.com>
2023-10-08 21:01:23 +0800

    try out wechaty

--- wechaty-playground/index.js
+++ wechaty-playground/index.js
@@ -1,1 +1,18 @@
-console.log("hello world")
+import { WechatyBuilder } from 'wechaty'
+
+const wechaty = WechatyBuilder.build() // get a Wechaty instance
+
+async function onMessage(msg) {
+  console.log('StarterBot', msg.toString())
+  console.log('msg body', msg)
+
+  if (msg.text() === 'ding') {
+    await msg.say('dong')
+  }
+}
+
+wechaty
+  .on('scan', (qrcode, status) => console.log(`Scan QR Code to login: ${status}\nhttps://wechaty.js.org/qrcode/${encodeURIComponent(qrcode)}`))
+  .on('login',            user => console.log(`User ${user} logged in`))
+  .on('message',       onMessage)
+wechaty.start()
