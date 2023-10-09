de8aa313c399d59b03931fa30fae9af475045c66
Karl Xu <karl.yogi.xu@gmail.com>
2023-10-09 23:29:41 +0800

    #4 fix variable name

--- wechaty-playground/index.js
+++ wechaty-playground/index.js
@@ -4,7 +4,7 @@ const wechaty = WechatyBuilder.build() // get a Wechaty instance
 
 async function onMessage(msg) {
   console.log('StarterBot', msg.toString())
-  console.log('msg room body', message.room())
+  console.log('msg room body', msg.room())
 
   if (msg.text() === 'ding') {
     await msg.say('dong')
